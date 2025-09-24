6dd897a500d2fef027ec968d15fab085e4767e3e
Andrew <sokolov3indastris@gmail.com>
2025-09-25 00:17:24 +0300

    [Feature] Added extra attribures

--- src/program/main.cpp
+++ src/program/main.cpp
@@ -2,48 +2,13 @@
 #include "svg.h"
 
 #include <cmath>
-#include <iostream>
-#include <memory>
-#include <vector>
 
-using namespace std::literals;
-using namespace svg;
 
-namespace shapes {
+namespace {
 
-class Triangle : public svg::Drawable {
-public:
-    Triangle(svg::Point p1, svg::Point p2, svg::Point p3)
-        : p1_(p1)
-        , p2_(p2)
-        , p3_(p3) {
-    }
-
-    // Реализует метод Draw интерфейса svg::Drawable
-    void Draw(svg::ObjectContainer& container) const override {
-        container.Add(svg::Polyline().AddPoint(p1_).AddPoint(p2_).AddPoint(p3_).AddPoint(p1_));
-    }
-
-private:
-    svg::Point p1_, p2_, p3_;
-};
-
-class Star : public svg::Drawable {
-public:
-    Star(svg::Point center, double outer_rad, double inner_rad, int num_rays)
-        : center_(center)
-        , outer_rad_(outer_rad)
-        , inner_rad_(inner_rad)
-        , num_rays_(num_rays) {
-    }
-
-    void Draw(svg::ObjectContainer& container) const override {
-        container.Add(CreateStar(center_, outer_rad_, inner_rad_, num_rays_));
-    }
-
-private:
-    svg::Polyline CreateStar(svg::Point center, double outer_rad, double inner_rad, int num_rays) const {
-        svg::Polyline polyline;
+    svg::Polyline CreateStar(svg::Point center, double outer_rad, double inner_rad, int num_rays) {
+        using namespace svg;
+        Polyline polyline;
         for (int i = 0; i <= num_rays; ++i) {
             double angle = 2 * M_PI * (i % num_rays) / num_rays;
             polyline.AddPoint({center.x + outer_rad * sin(angle), center.y - outer_rad * cos(angle)});
@@ -56,42 +21,81 @@ private:
         return polyline;
     }
 
-    svg::Point center_;
-    double outer_rad_;
-    double inner_rad_;
-    int num_rays_;
-};
+}  // namespace
 
-class Snowman : public svg::Drawable {
-public:
-    Snowman(svg::Point head_center, double head_radius)
-        : head_center_(head_center)
-        , head_radius_(head_radius) {
-    }
+namespace shapes {
 
-    void Draw(svg::ObjectContainer& container) const override {
-        double middle_radius = head_radius_ * 1.5;
-        double bottom_radius = head_radius_ * 2.0;
-        
-        // Расчет центров по примеру из условия:
-        // Средний круг: центр находится ниже головы на 2 * head_radius_
-        svg::Point middle_center = {head_center_.x, head_center_.y + 2 * head_radius_};
-        // Нижний круг: центр находится ниже головы на 5 * head_radius_
-        svg::Point bottom_center = {head_center_.x, head_center_.y + 5 * head_radius_};
-        
-        // Рисуем снизу вверх
-        container.Add(svg::Circle().SetCenter(bottom_center).SetRadius(bottom_radius));
-        container.Add(svg::Circle().SetCenter(middle_center).SetRadius(middle_radius));
-        container.Add(svg::Circle().SetCenter(head_center_).SetRadius(head_radius_));
-    }
+    class Star : public svg::Drawable {
+    public:
+        Star(svg::Point center, double outer_radius, double inner_radius, int num_rays)
+                : center_(center)
+                , outer_radius_(outer_radius)
+                , inner_radius_(inner_radius)
+                , num_rays_(num_rays) {
+        }
 
-private:
-    svg::Point head_center_;
-    double head_radius_;
-};
-} // namespace shapes
+        void Draw(svg::ObjectContainer& container) const override {
+            using namespace std::literals;
+            container.Add(  //
+                    CreateStar(center_, outer_radius_, inner_radius_, num_rays_)
+                            .SetFillColor("red"s)
+                            .SetStrokeColor("black"s));
+        }
 
-namespace {
+    private:
+        svg::Point center_;
+        double outer_radius_;
+        double inner_radius_;
+        int num_rays_;
+    };
+
+    class Snowman : public svg::Drawable {
+    public:
+        Snowman(svg::Point head_center, double head_radius)
+                : head_center_(head_center)
+                , head_radius_(head_radius) {
+        }
+
+        void Draw(svg::ObjectContainer& container) const override {
+            using namespace svg;
+            using namespace std::literals;
+
+            const auto base_circle = Circle().SetFillColor("rgb(240,240,240)"s).SetStrokeColor("black");
+            container.Add(  //
+                    Circle(base_circle)
+                            .SetCenter({head_center_.x, head_center_.y + 5 * head_radius_})
+                            .SetRadius(2 * head_radius_));
+
+            container.Add(  //
+                    Circle(base_circle)
+                            .SetCenter({head_center_.x, head_center_.y + 2 * head_radius_})
+                            .SetRadius(1.5 * head_radius_));
+            container.Add(Circle(base_circle).SetCenter(head_center_).SetRadius(head_radius_));
+        }
+
+    private:
+        svg::Point head_center_;
+        double head_radius_;
+    };
+
+    class Triangle : public svg::Drawable {
+    public:
+        Triangle(svg::Point p1, svg::Point p2, svg::Point p3)
+                : p1_(p1)
+                , p2_(p2)
+                , p3_(p3) {
+        }
+
+        // Реализует метод Draw интерфейса svg::Drawable
+        void Draw(svg::ObjectContainer& container) const override {
+            container.Add(svg::Polyline().AddPoint(p1_).AddPoint(p2_).AddPoint(p3_).AddPoint(p1_));
+        }
+
+    private:
+        svg::Point p1_, p2_, p3_;
+    };
+
+}  // namespace shapes
 
 template <typename DrawableIterator>
 void DrawPicture(DrawableIterator begin, DrawableIterator end, svg::ObjectContainer& target) {
@@ -106,26 +110,32 @@ void DrawPicture(const Container& container, svg::ObjectContainer& target) {
     DrawPicture(begin(container), end(container), target);
 }
 
-}  // namespace
-
 int main() {
     using namespace svg;
     using namespace shapes;
     using namespace std;
 
     vector<unique_ptr<svg::Drawable>> picture;
-
     picture.emplace_back(make_unique<Triangle>(Point{100, 20}, Point{120, 50}, Point{80, 40}));
-    // 5-лучевая звезда с центром {50, 20}, длиной лучей 10 и внутренним радиусом 4
     picture.emplace_back(make_unique<Star>(Point{50.0, 20.0}, 10.0, 4.0, 5));
-    // Снеговик с "головой" радиусом 10, имеющей центр в точке {30, 20}
     picture.emplace_back(make_unique<Snowman>(Point{30, 20}, 10.0));
 
     svg::Document doc;
-    // Так как документ реализует интерфейс ObjectContainer,
-    // его можно передать в DrawPicture в качестве цели для рисования
     DrawPicture(picture, doc);
 
-    // Выводим полученный документ в stdout
+    const Text base_text =  //
+            Text()
+                    .SetFontFamily("Verdana"s)
+                    .SetFontSize(12)
+                    .SetPosition({10, 100})
+                    .SetData("Happy New Year!"s);
+    doc.Add(Text{base_text}
+                    .SetStrokeColor("yellow"s)
+                    .SetFillColor("yellow"s)
+                    .SetStrokeLineJoin(StrokeLineJoin::ROUND)
+                    .SetStrokeLineCap(StrokeLineCap::ROUND)
+                    .SetStrokeWidth(3));
+    doc.Add(Text{base_text}.SetFillColor("red"s));
+
     doc.Render(cout);
 }
--- src/svg/svg.cpp
+++ src/svg/svg.cpp
@@ -2,139 +2,194 @@
 
 namespace svg {
 
-using namespace std::literals;
+    using namespace std::literals;
 
-void Object::Render(const RenderContext& context) const {
-    context.RenderIndent();
-
-    // Делегируем вывод тега своим подклассам
-    RenderObject(context);
-
-    context.out << std::endl;
-}
-
-// ---------- Circle ------------------
-
-Circle& Circle::SetCenter(Point center)  {
-    center_ = center;
-    return *this;
-}
-
-Circle& Circle::SetRadius(double radius)  {
-    radius_ = radius;
-    return *this;
-}
-
-void Circle::RenderObject(const RenderContext& context) const {
-    auto& out = context.out;
-    out << "<circle cx=\""sv << center_.x << "\" cy=\""sv << center_.y << "\" "sv;
-    out << "r=\""sv << radius_ << "\" "sv;
-    out << "/>"sv;
-}
-
-// ---------- Polyline ------------------
-
-Polyline& Polyline::AddPoint(Point point) {
-    points_.push_back(point);
-    return *this;
-}
-
-void Polyline::RenderObject(const RenderContext& context) const {
-    auto& out = context.out;
-    out << "<polyline points=\""sv;
-    
-    bool first = true;
-    for (const auto& point : points_) {
-        if (!first) {
-            out << " "sv;
+    std::ostream& operator<<(std::ostream& out, StrokeLineCap value) {
+        std::string_view sv;
+        switch (value) {
+            case StrokeLineCap::BUTT:
+                sv = "butt"sv;
+                break;
+            case StrokeLineCap::ROUND:
+                sv = "round"sv;
+                break;
+            case StrokeLineCap::SQUARE:
+                sv = "square"sv;
+                break;
         }
-        out << point.x << ","sv << point.y;
-        first = false;
+        return out << sv;
     }
-    
-    out << "\" />"sv;
-}
 
-// ---------- Text ------------------
-
-Text& Text::SetPosition(Point pos) {
-    position_ = pos;
-    return *this;
-}
-
-Text& Text::SetOffset(Point offset) {
-    offset_ = offset;
-    return *this;
-}
-
-Text& Text::SetFontSize(uint32_t size) {
-    font_size_ = size;
-    return *this;
-}
-
-Text& Text::SetFontFamily(std::string font_family) {
-    font_family_ = std::move(font_family);
-    return *this;
-}
-
-Text& Text::SetFontWeight(std::string font_weight) {
-    font_weight_ = std::move(font_weight);
-    return *this;
-}
-
-Text& Text::SetData(std::string data) {
-    data_ = std::move(data);
-    return *this;
-}
-
-std::string Text::EscapeText(const std::string& text) {
-    std::string result;
-    for (char c : text) {
-        switch (c) {
-            case '"': result += "&quot;"sv; break;
-            case '\'': result += "&apos;"sv; break;
-            case '<': result += "&lt;"sv; break;
-            case '>': result += "&gt;"sv; break;
-            case '&': result += "&amp;"sv; break;
-            default: result += c; break;
+    std::ostream& operator<<(std::ostream& out, StrokeLineJoin value) {
+        std::string_view sv;
+        switch (value) {
+            case StrokeLineJoin::ARCS:
+                sv = "arcs"sv;
+                break;
+            case StrokeLineJoin::BEVEL:
+                sv = "bevel"sv;
+                break;
+            case StrokeLineJoin::MITER:
+                sv = "miter"sv;
+                break;
+            case StrokeLineJoin::MITER_CLIP:
+                sv = "miter-clip"sv;
+                break;
+            case StrokeLineJoin::ROUND:
+                sv = "round"sv;
+                break;
         }
+        return out << sv;
     }
-    return result;
-}
 
-void Text::RenderObject(const RenderContext& context) const {
-    auto& out = context.out;
-    out << "<text x=\""sv << position_.x << "\" y=\""sv << position_.y << "\" "sv;
-    out << "dx=\""sv << offset_.x << "\" dy=\""sv << offset_.y << "\" "sv;
-    out << "font-size=\""sv << font_size_ << "\" "sv;
-    
-    if (!font_family_.empty()) {
-        out << "font-family=\""sv << font_family_ << "\" "sv;
+    void Object::Render(const RenderContext& context) const {
+        context.RenderIndent();
+
+        // Делегируем вывод тэга своим подклассам
+        RenderObject(context);
+
+        context.out << std::endl;
     }
-    
-    if (!font_weight_.empty()) {
-        out << "font-weight=\""sv << font_weight_ << "\" "sv;
+
+// Circle
+
+    Circle& Circle::SetCenter(Point center) {
+        center_ = center;
+        return *this;
     }
-    
-    out << ">"sv << EscapeText(data_) << "</text>"sv;
-}
 
-// ---------- Document ------------------
-
-void Document::AddPtr(std::unique_ptr<Object>&& obj) {
-    objects_.push_back(std::move(obj));
-}
-
-void Document::Render(std::ostream& out) const {
-    out << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"sv << std::endl;
-    out << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"sv << std::endl;
-    
-    RenderContext ctx(out, 2);
-    for (const auto& obj : objects_) {
-        obj->Render(ctx.Indented());
+    Circle& Circle::SetRadius(double radius) {
+        radius_ = radius;
+        return *this;
     }
-    
-    out << "</svg>"sv;
-}
+
+    void Circle::RenderObject(const RenderContext& context) const {
+        auto& out = context.out;
+        out << "<circle cx=\""sv << center_.x << "\" cy=\""sv << center_.y << "\" "sv;
+        out << "r=\""sv << radius_ << "\" "sv;
+        RenderAttrs(out);
+        out << "/>"sv;
+    }
+
+// Polyline
+
+    Polyline& Polyline::AddPoint(Point point) {
+        points_.push_back(point);
+        return *this;
+    }
+
+    void Polyline::RenderObject(const RenderContext& context) const {
+        auto& out = context.out;
+        out << "<polyline points=\""sv;
+        bool first = true;
+        for (const Point& p : points_) {
+            if (first) {
+                first = false;
+            } else {
+                out << ' ';
+            }
+            out << p.x << ',' << p.y;
+        }
+        out << "\" "sv;
+        RenderAttrs(out);
+        out << "/>"sv;
+    }
+
+// Text
+
+    Text& Text::SetPosition(Point pos) {
+        position_ = pos;
+        return *this;
+    }
+
+    Text& Text::SetOffset(Point offset) {
+        offset_ = offset;
+        return *this;
+    }
+
+    Text& Text::SetFontSize(uint32_t size) {
+        font_size_ = size;
+        return *this;
+    }
+
+    Text& Text::SetFontFamily(std::string font_family) {
+        font_family_ = std::move(font_family);
+        return *this;
+    }
+
+    Text& Text::SetFontWeight(std::string font_weight) {
+        font_weight_ = std::move(font_weight);
+        return *this;
+    }
+
+    Text& Text::SetData(std::string data) {
+        data_ = std::move(data);
+        return *this;
+    }
+
+    void Text::RenderObject(const RenderContext& context) const {
+        auto& out = context.out;
+        out << "<text "sv;
+        RenderAttrs(out);
+        using detail::RenderAttr;
+        RenderAttr(out, " x"sv, position_.x);
+        RenderAttr(out, " y"sv, position_.y);
+        RenderAttr(out, " dx"sv, offset_.x);
+        RenderAttr(out, " dy"sv, offset_.y);
+        RenderAttr(out, " font-size"sv, font_size_);
+        if (!font_family_.empty()) {
+            RenderAttr(out, " font-family"sv, font_family_);
+        }
+        if (!font_weight_.empty()) {
+            RenderAttr(out, " font-weight"sv, font_weight_);
+        }
+        out.put('>');
+        detail::HtmlEncodeString(out, data_);
+        out << "</text>"sv;
+    }
+
+// Document
+
+    void Document::AddPtr(std::unique_ptr<Object>&& obj) {
+        objects_.push_back(std::move(obj));
+    }
+
+    void Document::Render(std::ostream& out) const {
+        out << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"sv << std::endl;
+        out << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"sv << std::endl;
+        RenderContext ctx{out, 2, 2};
+        for (const auto& obj : objects_) {
+            obj->Render(ctx);
+        }
+        out << "</svg>"sv;
+    }
+
+    namespace detail {
+
+        void HtmlEncodeString(std::ostream& out, std::string_view sv) {
+            for (char c : sv) {
+                switch (c) {
+                    case '"':
+                        out << "&quot;"sv;
+                        break;
+                    case '<':
+                        out << "&lt;"sv;
+                        break;
+                    case '>':
+                        out << "&gt;"sv;
+                        break;
+                    case '&':
+                        out << "&amp;"sv;
+                        break;
+                    case '\'':
+                        out << "&apos;"sv;
+                        break;
+                    default:
+                        out.put(c);
+                }
+            }
+        }
+
+    }  // namespace detail
 
 }  // namespace svg
